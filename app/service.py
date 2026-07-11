b2534872ce60a0363a2df72e0d72aeecc2d2fcc6
Anna9793 <anna.w.bielska@gmail.com>
2026-07-11 17:06:21 +0200

    Implemented predict_churn_service in service.py  to load the production model from MLflow, compute predictions, and output probabilities.

--- app/service.py
+++ app/service.py
@@ -10,6 +10,7 @@ mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "file:./mlruns"))
 PROD_MODEL_URI = "models:/customer_segmentation_model@production"
 
 
+
 prod_pipeline = mlflow.sklearn.load_model(PROD_MODEL_URI)
 
 client = MlflowClient()
@@ -20,6 +21,15 @@ version_info = client.get_model_version_by_alias(
 )
 
 MODEL_VERSION = version_info.version
+
+PROD_CHURN_MODEL_URI = "models:/customer_churn_model@production"
+prod_churn_pipeline = mlflow.sklearn.load_model(PROD_CHURN_MODEL_URI)
+
+churn_version_info = client.get_model_version_by_alias(
+    "customer_churn_model",
+    "production"
+)
+CHURN_MODEL_VERSION = churn_version_info.version
 
 profile_df = pd.read_csv(CLUSTER_PROFILE)
 
@@ -38,3 +48,13 @@ def predict_cluster(features: dict):
     label = cluster_labels.get(cluster, "Unknown")
     
     return cluster, label
+
+def predict_churn_service(features: dict):
+
+    df = pd.DataFrame([features])
+
+    is_churn = int(prod_churn_pipeline.predict(df)[0])
+    churn_probability = float(prod_churn_pipeline.predict_proba(df)[0][1])
+
+    return is_churn, churn_probability
+
