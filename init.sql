f0d87dc3e3e6a6e7240c1e04ab3d364d38081dd0
Anna9793 <anna.w.bielska@gmail.com>
2026-07-11 15:56:25 +0200

    adding new table churn_predictions

--- init.sql
+++ init.sql
@@ -11,3 +11,17 @@ CREATE TABLE IF NOT EXISTS predictions (
     response_time_ms DOUBLE PRECISION,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS churn_predictions (
+    request_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    recency DOUBLE PRECISION,
+    frequency INT,
+    avg_order_value DOUBLE PRECISION,
+    churn_probability DOUBLE PRECISION,
+    is_churn INT,
+    model_version VARCHAR(20),
+    feature_version VARCHAR(20),
+    response_time_ms DOUBLE PRECISION,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
